338ebf3687fa6f09c37ba8d62f7b3ac0fa55fd24
Rishiraj Purohit <rishiraj.purohit@internations.org>
2020-10-10 00:19:36 +0200

    Add helmet ⛑

--- src/App.js
+++ src/App.js
@@ -3,10 +3,15 @@ import AppNavBar from "./partials/AppNavbar";
 import AppHeader from "./partials/AppHeader";
 import ReadMeGrid from "./components/ReadMeGrid";
 import ReadMeList from "./ReadMeList";
+import Constant from "./Constants";
 
 export default function App() {
   return (
     <>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>{Constant.APP_NAME}</title>
+      </Helmet>
       <AppNavBar />
       <AppHeader />
       <ReadMeGrid readmelist={ReadMeList}></ReadMeGrid>
